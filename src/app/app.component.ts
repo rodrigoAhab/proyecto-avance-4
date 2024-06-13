18b23681261e9ff6661203fad1b1a4cff5f62fff
rodrigoAhab <rodrigo.marin@unmsm.edu.pe>
2024-06-13 00:18:59 -0500

    Proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarSimpleComponent } from './navbar-simple/navbar-simple.component';
+import { TestComponent } from './test/test.component';
+import { LoginComponent } from './login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarSimpleComponent, TestComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { PrincipalComponent } from './principal/principal.component';
+import { LoginComponent } from './login/login.component';
+import { TestComponent } from './test/test.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/principal', pathMatch: 'full' }, //Indicamos que ni bien se ejecuta,se tenga la pagina1 de entrada
+    { path: 'principal', component: PrincipalComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'test', component: TestComponent}
+];
